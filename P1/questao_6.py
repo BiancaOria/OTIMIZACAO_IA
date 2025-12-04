eb599bf329d7c54407d8daa19101440336b3f9b2
Bianca Oria <biancaoria@gmail.com>
2025-12-04 11:59:43 -0300

    Q6

---
+++ P1/questao_6.py
@@ -0,0 +1,21 @@
+from busca_discreta import GlobalRandomSearch, LocalRandomSearch
+from hill import Hill
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    x1 = x[0]
+    x2 = x[1]
+    return x1 * np.sin(4 * np.pi * x1) - x2 * np.sin(4 * np.pi * x2 + np.pi) + 1
+
+
+# grs = GlobalRandomSearch(target_func=f, max_it=500, epsilon=0.5, opt='max', lim_inf=np.array([-1, -1]), lim_sup=np.array([3, 3]))
+# grs.search()
+
+
+# lrs = LocalRandomSearch(target_func=f, max_it=500, sigma=0.999999999, opt='max', lim_inf=np.array([-1, -1]), lim_sup=np.array([3, 3]))
+# lrs.search() 
+
+
+# hill = Hill(target_func=f, max_it=500, sigma=0.999999999999999999999999999, max_vizinhos=400, opt='max', lim_inf=np.array([-1, -1]), lim_sup=np.array([3, 3]))
+# hill.search()

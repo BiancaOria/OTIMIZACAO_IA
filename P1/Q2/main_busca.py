05c04b46f68564020725fe6e29b8420afd1e89a7
Bianca Oria <biancaoria@gmail.com>
2025-12-04 08:17:26 -0300

    oi

---
+++ P1/Q2/main_busca.py
@@ -0,0 +1,15 @@
+from busca_discreta import GlobalRandomSearch, LocalRandomSearch
+from hill import Hill
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+grs = GlobalRandomSearch(1000,.5)
+grs.search()
+
+# lrs = LocalRandomSearch(1000,0.5)
+# lrs.search()
+# plt.show()
+# hill = Hill(1000,0.5,20)
+# hill.search()
